f423f2d3597271297b7c798c1d960ebbc6ec7a0f
glennmichael123 <gtorregosa@gmail.com>
2025-06-12 12:22:15 +0800

    chore: wip

--- storage/framework/core/payments/src/drivers/stripe.ts
+++ storage/framework/core/payments/src/drivers/stripe.ts
@@ -1,6 +1,7 @@
+import { config } from '@stacksjs/config'
 import Stripe from 'stripe'
 
-const apiKey = 'dsdsd'
+const apiKey = config.services.stripe?.secretKey || 'abcd'
 
 const client = new Stripe(apiKey, {
   apiVersion: '2025-02-24.acacia',
