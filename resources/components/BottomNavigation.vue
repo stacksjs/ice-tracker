5d927f1cb7830e518876a75869e35aaeb1125db0
glennmichael123 <gtorregosa@gmail.com>
2025-06-12 22:27:10 +0800

    chore: wip

--- resources/components/BottomNavigation.vue
+++ resources/components/BottomNavigation.vue
@@ -1,10 +1,16 @@
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+</script>
+
 <template>
   <nav class="bg-white border-t border-gray-200 safe-bottom">
     <div class="flex justify-around items-center h-16">
       <RouterLink
         to="/map"
         class="flex flex-col items-center px-4 py-2"
-        :class="{ 'text-teal-600': $route.path === '/map' }"
+        :class="{ 'text-teal-600': route.path === '/map' }"
       >
         <div i-hugeicons-maps-circle-01 class="text-2xl" />
         <span class="text-xs font-serif">Map</span>
@@ -13,7 +19,7 @@
       <RouterLink
         to="/activity"
         class="flex flex-col items-center px-4 py-2 relative"
-        :class="{ 'text-teal-600': $route.path === '/activity' }"
+        :class="{ 'text-teal-600': route.path === '/activity' }"
       >
         <div i-hugeicons-left-to-right-list-bullet class="text-2xl" />
         <span class="text-xs font-sans">Activity</span>
@@ -31,7 +37,7 @@
       <RouterLink
         to="/about"
         class="flex flex-col items-center px-4 py-2 relative"
-        :class="{ 'text-teal-600': $route.path === '/about' }"
+        :class="{ 'text-teal-600': route.path === '/about' }"
       >
         <div i-hugeicons-user-group class="text-2xl" />
         <span class="text-xs font-sans">About</span>
